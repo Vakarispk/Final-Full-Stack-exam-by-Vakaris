251c9694e81ab248566598dbbc50bd20b35c2dd0
Vakarispk <vakarispaukste@email.com>
2022-10-09 15:47:46 +0300

    created admin login template

---
+++ notebook/note/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Inscription, Category
+
+class InscriptionAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category',)
+    search_fields = ('title','category__name',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'display_inscriptions',)
+
+admin.site.register(Inscription, InscriptionAdmin)
+admin.site.register(Category, CategoryAdmin)
+
---
+++ notebook/note/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from tinymce.models import HTMLField
+
+class Inscription(models.Model):
+    title = models.CharField('Pavadinimas', max_length=200)
+    text = models.TextField('Aprašymas', max_length=1000, help_text='Užrašo aprašymas')
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
+  
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Užrašas'
+        verbose_name_plural = 'Užrašai'
+
+class Category(models.Model):
+    name = models.CharField('Pavadinimas', max_length=200, help_text='Įveskite užrašo kategoriją')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Kategorija'
+        verbose_name_plural = 'Kategorijos'
+
+    def display_inscriptions(self):
+        return ', '.join(inscription.title for inscription in self.inscription_set.all()[:3])
+
+    display_inscriptions.short_description = 'Knygos'
